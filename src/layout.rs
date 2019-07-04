b449f94a76a2d4b90633537c4b75d01a877a0c4f
Nate Yazdani <nyazdani@cs.washington.edu>
2019-07-04 10:02:24 -0700

    Simplify control flow in layout traversal

--- src/layout.rs
+++ src/layout.rs
@@ -1,10 +1,29 @@
 ///! Basic CSS block layout.
 
-use style::{StyledNode, Style, Display, Edge, Automatic, Pixels};
+use style::{StyledNode, Style, Display, Edge, Pixels};
 use paint::{DisplayList, DisplayCommand};
 use std::default::Default;
 
 // CSS box model. All sizes are in px.
+
+#[derive(Clone, Copy, Default, Debug)]
+struct Rect {
+    x: Pixels,
+    y: Pixels,
+    width: Pixels,
+    height: Pixels,
+}
+
+impl Rect {
+    pub fn expanded_by(self, edge: Edge<Pixels>) -> Rect {
+        Rect {
+            x: self.x - edge.left,
+            y: self.y - edge.top,
+            width: self.width + edge.left + edge.right,
+            height: self.height + edge.top + edge.bottom,
+        }
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum BoxType {
@@ -20,6 +39,8 @@ enum BoxType {
 pub struct LayoutBox<'a> {
     /// Position and size of the content box relative to the document origin.
     content: Rect,
+    /// Position and (mainly) size ignoring any adjustments due to style constraints.
+    intrinsic: Rect,
     /// Position and size of the container box (from the containing block).
     container: Rect,
     /// Edges of the padding box.
@@ -28,6 +49,8 @@ pub struct LayoutBox<'a> {
     border: Edge<Pixels>,
     /// Edges of the margin box.
     margin: Edge<Pixels>,
+    /// Excess (or missing) horizontal space.
+    underflow: Pixels,
     /// Specified values from styling.
     style: &'a Style,
     /// Whether this box is anonymous.
@@ -42,10 +65,12 @@ impl<'a> LayoutBox<'a> {
     fn new(box_type: BoxType, style: &'a Style) -> Self {
         LayoutBox {
             content: Rect::default(),
+            intrinsic: Rect::default(),
             container: Rect::default(),
             padding: Edge::default(),
             border: Edge::default(),
             margin: Edge::default(),
+            underflow: 0.0,
             style: style,
             anonymous: true,
             box_type: box_type,
@@ -134,15 +159,42 @@ impl<'a> LayoutBox<'a> {
         // laying out its children.
         self.calculate_block_width();
 
-        // Determine where the box is located within its container.
-        self.calculate_block_position();
+        // Finish calculating the block's edge sizes, and position it within its containing block.
+        self.margin.top = self.style.margin.top.value(); // auto ==> 0
+        self.margin.bottom = self.style.margin.bottom.value(); // auto ==> 0
+
+        self.border.top = self.style.border.top;
+        self.border.bottom = self.style.border.bottom;
+
+        self.padding.top = self.style.padding.top;
+        self.padding.bottom = self.style.padding.bottom;
+
+        // Position the box flush left (w.r.t. margin/border/padding) to the container.
+        self.intrinsic.x = self.container.x +
+                           self.margin.left + self.border.left + self.padding.left;
+        self.content.x = self.intrinsic.x;
+
+        // Position the box below all the previous boxes in the container.
+        self.intrinsic.y = self.container.y + self.container.height +
+                           self.margin.top + self.border.top + self.padding.top;
+        self.content.y = self.intrinsic.y;
 
         // Recursively lay out the children of this box.
-        self.layout_block_children();
+        self.intrinsic.height = 0.0; // fold accumulator
+        for child in &mut self.children {
+            child.container = self.intrinsic;
+            child.layout();
+            // Increment the height so each child is laid out below the previous one.
+            self.intrinsic.height += child.margin_box().height;
+        }
 
         // Parent height can depend on child height, so `calculate_height` must be called after the
         // children are laid out.
-        self.calculate_block_height();
+        self.content.height = if self.style.height.is_auto() {
+            self.intrinsic.height
+        } else {
+            self.style.height.value()
+        };
     }
 
     /// Calculate the width of a block-level non-replaced element in normal flow.
@@ -151,64 +203,17 @@ impl<'a> LayoutBox<'a> {
     ///
     /// Sets the horizontal margin/padding/border dimensions, and the `width`.
     fn calculate_block_width(&mut self) {
-        let mut width = self.style.width;
-
-        let mut margin_left = self.style.margin.left;
-        let mut margin_right = self.style.margin.right;
-
-        let total: Pixels = [
-            margin_left.value(), margin_right.value(),
+        self.intrinsic.width = [
+            self.style.margin.left.value(), self.style.margin.right.value(),
             self.style.border.left, self.style.border.right,
             self.style.padding.left, self.style.padding.right,
-            width.value(),
+            self.style.width.value(),
         ].iter().sum();
-
-        // If width is not auto and the total is wider than the container, treat auto margins as 0.
-        if width.is_given() && total > self.container.width {
-            if margin_left.is_auto() {
-                margin_left = Automatic::Given(0.0);
-            }
-            if margin_right.is_auto() {
-                margin_right = Automatic::Given(0.0);
-            }
-        }
 
         // Adjust used values so that the above sum equals `containing_block.width`.
         // Each arm of the `match` should increase the total width by exactly `underflow`,
         // and afterward all values should be absolute lengths in px.
-        let underflow = self.container.width - total;
-
-        match (width.is_auto(), margin_left.is_auto(), margin_right.is_auto()) {
-            // If the values are overconstrained, calculate margin_right.
-            (false, false, false) => {
-                margin_right = Automatic::Given(margin_right.value() + underflow);
-            }
-
-            // If exactly one size is auto, its used value follows from the equality.
-            (false, false, true) => { margin_right = Automatic::Given(underflow); }
-            (false, true, false) => { margin_left  = Automatic::Given(underflow); }
-
-            // If width is set to auto, any other auto values become 0.
-            (true, _, _) => {
-                if margin_left.is_auto() { margin_left = Automatic::Given(0.0); }
-                if margin_right.is_auto() { margin_right = Automatic::Given(0.0); }
-
-                // Expand width to fill the underflow.
-                width = Automatic::Given(underflow.max(0.0));
-                if underflow < 0.0 {
-                    // Adjust the right margin to compensate for negative underflow.
-                    margin_right = Automatic::Given(margin_right.value() + underflow);
-                }
-            }
-
-            // If margin-left and margin-right are both auto, their used values are equal.
-            (false, true, true) => {
-                margin_left = Automatic::Given(underflow / 2.0);
-                margin_right = Automatic::Given(underflow / 2.0);
-            }
-        }
-
-        self.content.width = width.value();
+        self.underflow = self.container.width - self.intrinsic.width;
 
         self.padding.left = self.style.padding.left;
         self.padding.right = self.style.padding.right;
@@ -216,53 +221,39 @@ impl<'a> LayoutBox<'a> {
         self.border.left = self.style.border.left;
         self.border.right = self.style.border.right;
 
-        self.margin.left = margin_left.value();
-        self.margin.right = margin_right.value();
-    }
+        self.content.width = if self.style.width.is_auto() {
+            self.underflow.max(0.0)
+        } else {
+            self.style.width.value()
+        };
 
-    /// Finish calculating the block's edge sizes, and position it within its containing block.
-    ///
-    /// http://www.w3.org/TR/CSS2/visudet.html#normal-block
-    ///
-    /// Sets the vertical margin/padding/border dimensions, and the `x`, `y` values.
-    fn calculate_block_position(&mut self) {
-        // If margin-top or margin-bottom is `auto`, the used value is zero.
-        self.margin.top = self.style.margin.top.value();
-        self.margin.bottom = self.style.margin.bottom.value();
+        self.margin.left = if self.style.margin.left.is_auto() {
+            if self.style.width.is_auto() || self.underflow < 0.0 {
+                0.0
+            } else if self.style.margin.right.is_auto() {
+                self.underflow / 2.0
+            } else {
+                self.underflow
+            }
+        } else {
+            self.style.margin.left.value()
+        };
 
-        self.border.top = self.style.border.top;
-        self.border.bottom = self.style.border.bottom;
-
-        self.padding.top = self.style.padding.top;
-        self.padding.bottom = self.style.padding.bottom;
-
-        self.content.x = self.container.x +
-                         self.margin.left + self.border.left + self.padding.left;
-
-        // Position the box below all the previous boxes in the container.
-        self.content.y = self.container.y + self.container.height +
-                         self.margin.top + self.border.top + self.padding.top;
-    }
-
-    /// Lay out the block's children within its content area.
-    ///
-    /// Sets `self.dimensions.height` to the total content height.
-    fn layout_block_children(&mut self) {
-        for child in &mut self.children {
-            child.container = self.content;
-            child.layout();
-            // Increment the height so each child is laid out below the previous one.
-            self.content.height = self.content.height + child.margin_box().height;
-        }
-    }
-
-    /// Height of a block-level non-replaced element in normal flow with overflow visible.
-    fn calculate_block_height(&mut self) {
-        // If the height is set to an explicit length, use that exact length.
-        // Otherwise, just keep the value set by `layout_block_children`.
-        if let Automatic::Given(h) = self.style.height {
-            self.content.height = h;
-        }
+        self.margin.right = if self.style.width.is_auto() && self.underflow < 0.0 {
+            self.style.margin.right.value() + self.underflow
+        } else if self.style.margin.right.is_auto() {
+            if self.style.width.is_auto() {
+                0.0
+            } else if self.style.margin.left.is_auto() {
+                self.underflow / 2.0
+            } else {
+                self.underflow
+            }
+        } else if !self.style.margin.left.is_auto() || !self.style.width.is_auto() {
+            self.style.margin.right.value() + self.underflow
+        } else {
+            self.style.margin.right.value()
+        };
     }
 }
 
@@ -276,11 +267,9 @@ impl<'a> LayoutBox<'a> {
     }
 
     fn render_background(&self, list: &mut DisplayList) {
-        let color = self.style.background_color;
-
         let border_box = self.border_box();
         list.push(DisplayCommand::SolidColor {
-            color: color,
+            color: self.style.background_color,
             x: border_box.x,
             y: border_box.y,
             width: border_box.width,
@@ -289,13 +278,11 @@ impl<'a> LayoutBox<'a> {
     }
 
     fn render_borders(&self, list: &mut DisplayList) {
-        let color = self.style.border_color;
-
         let border_box = self.border_box();
 
         // Left border
         list.push(DisplayCommand::SolidColor {
-            color: color,
+            color: self.style.border_color,
             x: border_box.x,
             y: border_box.y,
             width: self.border.left,
@@ -304,7 +291,7 @@ impl<'a> LayoutBox<'a> {
 
         // Right border
         list.push(DisplayCommand::SolidColor {
-            color: color,
+            color: self.style.border_color,
             x: border_box.x + border_box.width - self.border.right,
             y: border_box.y,
             width: self.border.right,
@@ -313,7 +300,7 @@ impl<'a> LayoutBox<'a> {
 
         // Top border
         list.push(DisplayCommand::SolidColor{
-            color: color,
+            color: self.style.border_color,
             x: border_box.x,
             y: border_box.y,
             width: border_box.width,
@@ -322,30 +309,11 @@ impl<'a> LayoutBox<'a> {
 
         // Bottom border
         list.push(DisplayCommand::SolidColor {
-            color: color,
+            color: self.style.border_color,
             x: border_box.x,
             y: border_box.y + border_box.height - self.border.bottom,
             width: border_box.width,
             height: self.border.bottom,
         });
-    }
-}
-
-#[derive(Clone, Copy, Default, Debug)]
-struct Rect {
-    x: Pixels,
-    y: Pixels,
-    width: Pixels,
-    height: Pixels,
-}
-
-impl Rect {
-    pub fn expanded_by(self, edge: Edge<Pixels>) -> Rect {
-        Rect {
-            x: self.x - edge.left,
-            y: self.y - edge.top,
-            width: self.width + edge.left + edge.right,
-            height: self.height + edge.top + edge.bottom,
-        }
     }
 }
